c81c6d0885f05c173c23d4fa7b9faa47ed728cc4
Kayoko <mikasaopensource@gmail.com>
2025-02-08 00:08:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url' 
 
 global.owner = [
-  ['59172945992', 'FG98', true],
-  ['5491168352204']
+  ['6282138848994', 'Vyren', true],
+  ['6285711882963']
 ] //Numeros de owner 
 
 global.mods = [''] 
-global.prems = ['50489079501', '573143917092']
+global.prems = ['6285711882963', '6282138848994']
 global.APIs = { // API Prefix
   // name: 'https://website' 
   nrtm: 'https://fg-nrtm.ddns.net',
